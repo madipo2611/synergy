14801c3cecf0e87ac21c3eb54ec992aeca3e8e3c
madipo2611 <42010323+madipo2611@users.noreply.github.com>
2023-11-05 22:24:11 +0300

    Урок 2

---
+++ task_2.py
@@ -0,0 +1,17 @@
+print('Укажи этапы развития человека')
+print('1 этап:')
+#Australopithecus africanus
+a1 = input()
+print('2 этап:')
+#Homo habilis
+a2 = input()
+print('3 этап:')
+#Homo erectus
+a3 = input()
+print('4 этап:')
+#Homo Neanderthalensis
+a4 = input()
+print('5 этап:')
+#Homo Sapiens Sapiens
+a5 = input()
+print(a1, a2, a3, a4, a5, sep='=>')
